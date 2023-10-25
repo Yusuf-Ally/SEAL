3b7c8258120285f81fec5d8940ad9a3484b88b1c
Yusuf-Ally <allyyusuf25@gmail.com>
2023-10-25 02:09:38 +0200

    Added label extraction

--- sed_train.py
+++ sed_train.py
@@ -130,19 +130,34 @@ def move_data_to_device(x, device):
 
     return x.to(device)
 
-def load_data(_feat_folder, _lab_folder,  _fold=None):
+def load_data(_feat_folder, _lab_folder, _metadata_path,  _fold=None):
     # Load features (mbe)
-    feat_file_fold = os.path.join(_feat_folder, 'merged_mbe_fold{}.npz'.format( _fold))
-    dmp = np.load(feat_file_fold)
+    # feat_file_fold = os.path.join(_feat_folder, 'merged_mbe_fold{}.npz'.format( _fold))
+    # dmp = np.load(feat_file_fold)
+    #
+    # _X_train, _X_val = dmp['arr_0'], dmp['arr_1']
+    #
+    # # Load the corresponding labels
+    # lab_file_fold = os.path.join(_lab_folder, 'merged_lab_soft_fold{}.npz'.format(_fold))
+    # dmp = np.load(lab_file_fold)
+    # _Y_train, _Y_val = dmp['arr_0'], dmp['arr_1']
+    #
+    # return _X_train, _Y_train, _X_val, _Y_val
 
-    _X_train, _X_val = dmp['arr_0'], dmp['arr_1']
+    # load development features
+    development_file = os.path.join(_metadata_path, f'/crossvalidation/meta_fold0{_fold}_development.txt')
+    df = pd.read_csv(f'{_metadata_path}{development_file}',sep='\t')
+    unique_development_files = df['path'].unique()
 
-    # Load the corresponding labels
-    lab_file_fold = os.path.join(_lab_folder, 'merged_lab_soft_fold{}.npz'.format(_fold))
-    dmp = np.load(lab_file_fold)
-    _Y_train, _Y_val = dmp['arr_0'], dmp['arr_1']
+    # load development labels
 
-    return _X_train, _Y_train, _X_val, _Y_val
+    # load evaluation features
+    evaluation_file = os.path.join(_metadata_path, f'/crossvalidation/meta_fold0{_fold}_evaluation.txt')
+    df = pd.read_csv(f'{_metadata_path}{evaluation_file}', sep='\t')
+    unique_evaluation_files = df['path'].unique()
+
+    # load evaluation labels
+
 
 
 def preprocess_data(_X, _Y, _X_val, _Y_val, _seq_len):
@@ -158,9 +173,9 @@ def preprocess_data(_X, _Y, _X_val, _Y_val, _seq_len):
 
 
 def train():
-
+    #     Per fold:
     # 1. Generate X and Y data
-    # 2. Per fold, train model using MSE
+    # 2. Train model using MSE
     # 3. After training, evaluate on eval data using SED metrics
     # 4. After training on all folds and epochs for each fold, test on test metadata
 
@@ -197,38 +212,134 @@ def train():
         time_resolution=1.0
     )
 
-    for fold in config.cv_fold:
-        if fold == 1:
-            development_file = os.path.join(metadata_path,f'/crossvalidation/meta_fold0{fold}_development.txt')
-            # print(development_file)
-            df = pd.read_csv(f'{metadata_path}{development_file}',sep='\t')
-            # filenames = df['path'].str.rsplit('/', 1)
-            files = df['path'].unique()
-            # print(un)
-            print(files)
-
+    # Get all audiofiles
     # for fold in config.cv_fold:
-    #
-    #     # Load features and labels
-    #     X, Y, X_val, Y_val = load_data('development/features', 'development/soft_labels', fold)
-    #     X, Y, X_val, Y_val = preprocess_data(X, Y, X_val, Y_val, config.sequence_length)
-    #
-    #     train_dataset = TUTDataset(X, Y)
-    #     validate_dataset = TUTDataset(X_val, Y_val)
-    #
-    #     # Data loader
-    #     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,
-    #                                                num_workers=1, pin_memory=True)
-    #
-    #     validate_loader = torch.utils.data.DataLoader(dataset=validate_dataset, batch_size=batch_size, shuffle=True,
-    #                                                   num_workers=1, pin_memory=True)
-    #
-    #     # Prepare model
-    #     modelcrnn = CRNN(config.n_classes, config.cnn_filter, config.gru_hidden_layers, config.dropout)
-    #
-    #     if 'cuda' in device:
-    #         modelcrnn.to(device)
-    #     print('\nCreate model:')
+    #     if fold == 1:
+    #         development_file = os.path.join(metadata_path,f'/crossvalidation/meta_fold0{fold}_development.txt')
+    #         # print(development_file)
+    #         df = pd.read_csv(f'{metadata_path}{development_file}',sep='\t')
+    #         # filenames = df['path'].str.rsplit('/', 1)
+    #         files = df['path'].unique()
+    #         # print(un)
+    #         print(files)
+
+    for fold in config.cv_fold:
+
+        load_data('','', _metadata_path=metadata_path,_fold=fold)
+
+        # Load features and labels
+        # X, Y, X_val, Y_val = load_data('development/features', 'development/soft_labels', fold)
+        # X, Y, X_val, Y_val = preprocess_data(X, Y, X_val, Y_val, config.sequence_length)
+        #
+        # train_dataset = TUTDataset(X, Y)
+        # validate_dataset = TUTDataset(X_val, Y_val)
+        #
+        # # Data loader
+        # train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=config.batch_size, shuffle=True,
+        #                                            num_workers=1, pin_memory=True)
+        #
+        # validate_loader = torch.utils.data.DataLoader(dataset=validate_dataset, batch_size=config.batch_size, shuffle=True,
+        #                                               num_workers=1, pin_memory=True)
+        #
+        # # Prepare model
+        # modelcrnn = CRNN(config.n_classes, config.cnn_filter, config.gru_hidden_layers, config.dropout)
+        #
+        # if 'cuda' in device:
+        #     modelcrnn.to(device)
+        # print('\nCreate model:')
+        #
+        # # Optimizer
+        # optimizer = optim.Adam(CRNN.parameters(), lr=config.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0., amsgrad=False)
+        #
+        # best_epoch = 0; pat_cnt = 0; pat_learn_rate = 0; best_loss = 99999
+        # tr_loss, val_F1, val_ER = [0] * stop_iteration, [0] * stop_iteration, [0] * stop_iteration
+
+        # Train on mini batches
+        # tr_batch_loss = list()
+        # for epoch in range(stop_iteration):
+        #
+        #     modelcrnn.train()
+        #     # TRAIN
+        #     for (batch_data, batch_target) in train_loader:
+        #         # Zero gradients for every batch
+        #         optimizer.zero_grad()
+        #
+        #         batch_output = modelcrnn(move_data_to_device(batch_data, device))
+        #
+        #         # Calculate loss
+        #         loss = clip_mse(batch_output, move_data_to_device(batch_target, device))
+        #
+        #         tr_batch_loss.append(loss.item())
+        #
+        #         # Backpropagation
+        #         loss.backward()
+        #         optimizer.step()
+        #
+        #     tr_loss[epoch] = np.mean(tr_batch_loss)
+        #
+        #     # VALIDATE
+        #     modelcrnn.eval()
+        #
+        #     with torch.no_grad():
+        #
+        #         segment_based_metrics_batch = sed_eval.sound_event.SegmentBasedMetrics(
+        #             event_label_list=config.labels_hard,
+        #             time_resolution=1.0
+        #         )
+        #
+        #         running_loss = 0.0
+        #         for (batch_data, batch_target) in validate_loader:
+        #             batch_output = modelcrnn(move_data_to_device(batch_data, device))
+        #
+        #             loss = clip_mse(batch_output, move_data_to_device(batch_target, device))
+        #
+        #             segment_based_metrics_batch = metric_perbatch(segment_based_metrics_batch,
+        #                                                           batch_output.reshape(-1,
+        #                                                                                len(config.labels_soft)).detach().cpu().numpy(),
+        #                                                           batch_target.reshape(-1,
+        #                                                                                len(config.labels_soft)).numpy())
+        #
+        #             running_loss += loss
+        #
+        #         avg_vloss = running_loss / len(validate_loader)
+        #
+        #         batch_segment_based_metrics_ER = segment_based_metrics_batch.overall_error_rate()
+        #         batch_segment_based_metrics_f1 = segment_based_metrics_batch.overall_f_measure()
+        #         val_F1[epoch] = batch_segment_based_metrics_f1['f_measure']
+        #         val_ER[epoch] = batch_segment_based_metrics_ER['error_rate']
+        #
+        #         # Check if during the epochs the ER does not improve
+        #         if avg_vloss < best_loss:
+        #             best_model = modelcrnn
+        #             best_epoch = epoch
+        #             best_loss = avg_vloss
+        #             pat_cnt = 0
+        #             pat_learn_rate = 0
+        #             output = segment_based_metrics_batch.result_report_class_wise()
+        #
+        #             print(output)
+        #             torch.save(best_model.state_dict(), f'{output_model}/best_fold{fold}.bin')
+        #
+        #     pat_cnt += 1
+        #     pat_learn_rate += 1
+        #
+        #     if pat_learn_rate > int(0.3 * stop_iteration):
+        #         for g in optimizer.param_groups:
+        #             g['lr'] = g['lr'] / 10
+        #             pat_learn_rate = 0
+        #             print(f'\tDecreasing learning rate to:{g["lr"]}')
+        #
+        #     print(f'Epoch: {epoch} - Train loss: {round(tr_loss[epoch], 3)} - Val loss: {round(avg_vloss.item(), 3)}'
+        #           f' - val F1 {round(val_F1[epoch] * 100, 2)} - val ER {round(val_ER[epoch], 3)}'
+        #           f' - best epoch {best_epoch} F1 {round(val_F1[best_epoch] * 100, 2)}')
+        #
+        #     segment_based_metrics_batch.reset()
+        #     # Stop learning
+        #     if (epoch == stop_iteration) or (pat_cnt > patience):
+        #         break
+
+
+
 
 
 if __name__ == '__main__':
